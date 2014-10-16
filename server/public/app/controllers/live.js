33cebd9292558d14e63ba71054cc4c4b86640c63
martin <martin@rga.com>
2014-10-16 13:23:20 -0300

    much fe le tengo

--- server/public/app/controllers/live.js
+++ server/public/app/controllers/live.js
@@ -9,6 +9,12 @@ angular.module('ssApp.controllers')
       $http.get('live').
         success(function(data, status, headers, config) {
           $scope.liveVolume = data;
+          if (data.length > 0 ){
+            $scope.first = data[0];
+            if (data.length > 1){
+              $scope.last = data[data.length -1];
+            }
+          }
 
         }).
         error(function(data, status, headers, config) {
