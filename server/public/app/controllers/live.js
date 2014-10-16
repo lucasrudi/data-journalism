bc15c8085b5b2aabf9a2e87b1da417794a03360b
Lucas Rudi <Lucas.Rudi@rga.com>
2014-10-16 13:08:14 -0300

    test

--- server/public/app/controllers/live.js
+++ server/public/app/controllers/live.js
@@ -5,12 +5,12 @@ angular.module('ssApp.controllers')
   .controller('liveController', function($scope, $http, $routeParams, $location, $route, $interval) {
     
     $interval(function(){
+      $scope.liveVolume = [];
+      
       $http.get('live').
       success(function(data, status, headers, config) {
         $scope.liveVolume = data;
-        if(!$scope.$$phase) {
-          $scope.$apply();
-        }
+        
         
       }).
       error(function(data, status, headers, config) {
